c6a2f088d1c13d82f483f8212326bfc84ba995bc
Gulam Qasim <GQasim@spartaglobal.com>
2019-09-03 11:45:26 +0100

    second commit

--- Week4Homework/Program.cs
+++ Week4Homework/Program.cs
@@ -10,8 +10,10 @@ namespace IComparable_01
     {
 
         //Run the following code and step through it using breakpoints in order to understand what is happening
+        //Bonus Lab
         static void Main(string[] args)
         {
+            Comparison comp = new Comparison(); //Calling constructor
             var a = new Item();
             var b = new Item();
             //using the break point on a and b shows that they are null (absent of values)
@@ -19,20 +21,27 @@ namespace IComparable_01
             b.Name = "Carly";
             //till the var are assigned a name using the class item public string
             Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b));
+            comp.CompareByName(a.Name, b.Name);
             //Once it gets to this line the breakpoint moves onto the compareTo
             a.Name = "Carly";
             b.Name = "Carly";
             Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b));
+            comp.CompareByName(a.Name, b.Name);
+
             a.Name = "Edward";
             b.Name = "Carly";
             Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b));
+            comp.CompareByName(a.Name, b.Name);
+
+            Console.WriteLine(comp.CompareByLength("Testy","Hello"));      
         }
     }
-    public class Item /*:*/ /*IComparable*/
+    public class Item : IComparable
 
-        //The interface "IComparable" Can be commeted out without breaking the code
+    //The interface "IComparable" Can be commeted out without breaking the code
     {
         public string Name;
+
 
         public int CompareTo(object o)
         {
@@ -43,5 +52,57 @@ namespace IComparable_01
             //position in the sort orders as the other object. This creates the values at the top.
             return this.Name.CompareTo(that.Name);
         }
+    }
+    interface ICompareByName
+    {
+        int CompareByName(string StringA, string StringB);
+    }
+    interface ICompareByLength
+    {
+        int CompareByLength(string StringA, string StringB);
+    }
+
+    class Comparison : ICompareByLength, ICompareByName
+    {
+        public string Name { get; set; }
+
+        public int CompareByLength(string StringA, string StringB)
+        {
+            if (StringA.Length == StringB.Length)
+            {
+                Console.WriteLine("______________________________________________");
+                Console.WriteLine("String1 is the same length compared to String2");
+                return 0;
+            }
+            else if (StringA.Length > StringB.Length)
+            {
+                Console.WriteLine("String1 is longer compared to String2");
+                return 1;
+            }
+            else 
+            {
+                Console.WriteLine("String1 is lower compared to String2");
+                return -1;
+            }
+        }
+
+        public int CompareByName(string StringA, string StringB)
+        {
+            int storedNumber = string.Compare(StringA, StringB);
+
+            if (storedNumber == 0)
+            { 
+                Console.WriteLine("String1 is the same as String2 alphabetically");           
+            }
+            else if (storedNumber == -1)
+            {
+                Console.WriteLine("String1 is before as String2 alphabetically");
+            } else if (storedNumber == 1)
+            {
+                Console.WriteLine("String B comes before String A alphabetically");
+            }
+            return storedNumber;
+        }
+
     }
 }
